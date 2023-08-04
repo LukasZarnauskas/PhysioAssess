da57eb527c559b2761d8f5a80b753e6baffa5331
GluteusMedius <zarnauskas97@gmail.com>
2023-08-04 10:21:21 +0300

    nav added, react-router-dom installed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./styles/reset.css";
 import styled from "styled-components";
 
 const Container = styled.div`
-  background-color: #333;
+  background-color: #f4f4f4;
   color: whitesmoke;
 `;
 function App() {
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { styled } from "styled-components";
+import Nav from "../Nav";
+const HeaderNav = styled.header`
+  background-color: whitesmoke;
+  color: #0b3d91;
+`;
+const Logo = styled.p`
+  font-size: 2rem;
+  font-weight: bold;
+  padding: 0.5rem 0 0.5rem 0.5rem;
+`;
+const Span1 = styled.span`
+  color: #75c888;
+`;
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <HeaderNav>
+      <Logo>
+        Physio<Span1>Assess</Span1>
+        <Nav />
+      </Logo>
+    </HeaderNav>
+  );
 }
 
 export default Header;
